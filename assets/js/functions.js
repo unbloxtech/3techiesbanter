8033f37d63a1327775379172cb96e09d41e7f8dc
manujuniris <unbloxtech@gmail.com>
2023-01-11 17:29:46 +0530

    updated

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -212,3 +212,29 @@ jQuery( function() { "use strict";
 	}
 
 });
+
+$('#my-form').submit(function(e) {
+	//prevent the form from submiting so we can post to the google form
+	e.preventDefault();
+	//AJAX request
+	$.ajax({
+	  url: 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSebt1bd_JI65w2SDU1rHnPM9l0x-Dlq1BNdID2obxOJ7S_aGQ/formResponse',     //The public Google Form url, but replace /view with /formResponse
+	  data: $('#my-form').serialize(), //Nifty jquery function that gets all the input data 
+	  type: 'POST', //tells ajax to post the data to the url
+	  dataType: "json", //the standard data type for most ajax requests
+	  statusCode: { //the status code from the POST request
+		0: function(data) { //0 is when Google gives a CORS error, don't worry it went through
+		  //success
+		  $('#form-success').text('Thanks for contacting, we will revert soon.');
+		}, 
+		200: function(data) {//200 is a success code. it went through!
+		  //success
+		  $('#form-success').text('Thanks for contacting, we will revert soon.');
+		},
+		403: function(data) {//403 is when something went wrong and the submission didn't go through
+		  //error
+		  alert('Oh no! Something went wrong.');
+		}
+	  }  
+	});
+  });
